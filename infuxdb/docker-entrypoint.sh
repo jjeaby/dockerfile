159155b9172438938b3d2c0335fa57f92f6e4b5f
Lee YongJin <jjeaby@gmail.com>
2018-04-14 18:33:30 +0900

    infuxdb + influx-ui + grafana docker file

--- infuxdb/docker-entrypoint.sh
+++ infuxdb/docker-entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #cd /var/lib/influxdb/influxdb-ui
 
-#service grafana-server start
+# service grafana-server start
+### NOT starting grafana-server by default on bootup, please execute
+update-rc.d grafana-server defaults 95 10
+### In order to start grafana-server, execute
+# service grafana-server restart
+/etc/init.d/grafana-server restart
 
 
-influxd &
+
 cd /opt/influxdb-ui
-export PORT=4000
-yarn install  
+yarn install
+export PORT=4000 
 yarn start &
-
-service grafana-server restart
-
-
+influxd
 
 
 
